345a0cd26d01aa318539b79614a762cb8237248d
lkolla <lkolla@walmartlabs.com>
2015-09-07 00:03:08 -0700

    changes related to mongodb url

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ localStrategy(app)
 
 routes(app)
 
-mongoose.connect('mongodb://localhost:27017/wmt-blog')
+mongoose.connect('mongodb://localhost:27017/wmt-blogger')
 
 
 app.listen(HTTP_PORT, ()=>{
